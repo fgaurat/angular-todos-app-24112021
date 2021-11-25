b881bb80961a6321e623ca4f3d3b916748d57822
Frédéric Gaurat <fgaurat@eolem.com>
2021-11-25 11:29:12 +0100

    add env

---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+
+
+@NgModule({
+  declarations: [],
+  exports:[
+    MatTableModule
+  ]
+})
+export class MaterialModule { }
